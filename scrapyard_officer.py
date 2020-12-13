4a2b99443f4dfa8c58a6f55fec09f162dd24529a
Jakob Hauser <jakob.e.hauser@protonmail.com>
2020-12-13 16:49:43 +0100

    welcome to the scrapyard

--- scrapyard_officer.py
+++ scrapyard_officer.py
@@ -1,1 +1,43 @@
-import
+import schedule
+import time
+from telegram.ext import Updater, CommandHandler
+import logging
+
+from secrets import token
+
+garbage_status = False
+
+updater = Updater(token, use_context=True)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+def done():
+    global garbage_status
+    updater.bot.send_message(chat_id=-443613216, text="thanks next time do it a bit quicker")
+    garbage_status = True
+
+garbage_is_out_handler = CommandHandler('done', done)
+
+updater.dispatcher.add_handler(garbage_is_out_handler)
+
+def job():
+    global garbage_status
+    updater.start_polling()
+    updater.bot.send_message(chat_id=-443613216, text="it is sunday get your shit together and put out the garbage")
+
+    while not garbage_status:
+        updater.bot.send_message(chat_id=-443613216, text="lazy ass take out the garbage")
+        time.sleep(10)
+
+    updater.stop()
+    garbage_status = False
+
+
+
+
+
+
+    updater.stop()
+schedule.every().sunday.at("16:41").do(job)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
