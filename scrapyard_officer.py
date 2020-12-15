d596891ae95c9927a7cd5e47d9510719627524aa
Masetto96 <masetto.scola@gmail.com>
2020-12-15 15:01:20 +0100

    Final working version of scrapyard_officer

--- scrapyard_officer.py
+++ scrapyard_officer.py
@@ -1,43 +1,50 @@
 import schedule
 import time
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import Update
 import logging
 
 from secrets import token
 
-garbage_status = False
 
-updater = Updater(token, use_context=True)
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-def done():
+def done(update: Update, context: CallbackContext) -> None:
     global garbage_status
-    updater.bot.send_message(chat_id=-443613216, text="thanks next time do it a bit quicker")
     garbage_status = True
+    updater.bot.send_message(chat_id=-443613216, text="thanks (Y)")
 
-garbage_is_out_handler = CommandHandler('done', done)
 
-updater.dispatcher.add_handler(garbage_is_out_handler)
 
 def job():
     global garbage_status
     updater.start_polling()
-    updater.bot.send_message(chat_id=-443613216, text="it is sunday get your shit together and put out the garbage")
+    #updater.bot.send_message(chat_id=-443613216, text="it is sunday get your shit together and put out the garbage")
 
     while not garbage_status:
         updater.bot.send_message(chat_id=-443613216, text="lazy ass take out the garbage")
-        time.sleep(10)
-
-    updater.stop()
-    garbage_status = False
+        time.sleep(200)
 
 
 
 
 
 
-    updater.stop()
-schedule.every().sunday.at("16:41").do(job)
+
+garbage_status = False
+
+
+updater = Updater(token)
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+
+garbage_is_out_handler = CommandHandler('done', done)
+updater.dispatcher.add_handler(garbage_is_out_handler)
+
+
+schedule.every().sunday.at("19:00").do(job)
+
 while True:
     schedule.run_pending()
     time.sleep(1)
